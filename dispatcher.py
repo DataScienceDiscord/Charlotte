bd5aa5e1b8fa9dc733de091db91963bc2b0aec5c
Et5175 <et.dsd.5175@gmail.com>
2018-12-16 16:04:52 +0100

    Added docstrings for the dispatcher.

--- dispatcher.py
+++ dispatcher.py
@@ -3,6 +3,14 @@ import threading
 
 
 class Dispatcher(object):
+    """A dispatcher to relay the received messages to the appropriate
+    commands.
+
+    Args:
+        inc_queue: The incoming message queue.
+        database_connection: The datanase interface.
+        consumer: The discord REST API consumer to send messages through.
+    """
     COMMAND_PREFIX = "!c:"
 
     def __init__(self, inc_queue, database_connection, consumer):
@@ -12,9 +20,25 @@ class Dispatcher(object):
         self.running   = False
 
     def is_command(self, content):
+        """Indicates whether a message is a command.
+
+        Args:
+            content: The contents of the message.
+
+        Returns:
+            A boolean indicating whether the message is a command.
+        """
         return len(content) >= len(Dispatcher.COMMAND_PREFIX) and content[:3] == Dispatcher.COMMAND_PREFIX
 
     def parse(self, content):
+        """Parses a message to extract the command and its parameters.
+
+        Args:
+            content: The contents of the message.
+
+        Returns:
+            A tuple containing the command identifier and the parameters.
+        """
         if self.is_command(content):
 
             # Extract command
@@ -35,14 +59,27 @@ class Dispatcher(object):
             return command, params
         return None, None
 
-    def dispatch(self, command, params, message):
-        command_func = commands.identifiers[command]
-        return command_func(params, message)
+    def dispatch(self, command_id, params, message):
+        """Calls the appropriate command with the given parameters.
+
+        Args:
+            command_id: The string identifier of the command.
+            params: The parameters to be passed to the command.
+            message: The full message which contained the command.
+
+        Returns:
+            The results of the command.
+        """
+        command = commands.identifiers[command_id]
+        return command(params, message)
 
     def store(self, message):
         pass
 
     def run(self):
+        """Listens for new messages in the incoming queue and dispatches
+        them to the appropriate commands and stores them.
+        """
         while self.running:
             message = self.inc_queue.get()
 
@@ -58,9 +95,11 @@ class Dispatcher(object):
                 self.store(message)
 
     def start(self):
+        """Starts listening for incoming messages."""
         self.running = True
         t = threading.Thread(target=self.run)
         t.start()
 
     def stop(self):
+        """Stops listening for incoming messages."""
         self.running = False
