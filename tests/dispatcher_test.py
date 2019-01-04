647dd94b4b58b10a796205e9b7dac28faa01a605
Et5175 <et.dsd.5175@gmail.com>
2019-01-04 18:21:32 +0100

    Fixed tests for new delimiter

--- tests/dispatcher_test.py
+++ tests/dispatcher_test.py
@@ -5,38 +5,38 @@ from dispatcher import Dispatcher
 def test_is_command():
     ds = Dispatcher(queue.Queue(), None, None)
 
-    content = "!c:say:hello"
+    content = "!c/say/hello"
     assert ds.is_command(content)
 
-    content = "!c:dice"
+    content = "!c/dice"
     assert ds.is_command(content)
 
-    content = "#c:say:hello"
+    content = "#c/say/hello"
     assert not ds.is_command(content)
 
-    content = "c!:say:hello"
+    content = "c!/say/hello"
     assert not ds.is_command(content)
 
-    content = "sqegfqs<!::!cg"
+    content = "sqegfqs<!//!cg"
     assert not ds.is_command(content)
 
-    content = "sqegfqs!c:"
+    content = "sqegfqs!c/"
     assert not ds.is_command(content)
 
 def test_parse():
     ds = Dispatcher(queue.Queue(), None, None)
 
-    content = "!c:say:hello"
+    content = "!c/say/hello"
     command, params = ds.parse(content)
     assert command == "say"
     assert params == "hello"
 
-    content = "!c:dice"
+    content = "!c/dice"
     command, params = ds.parse(content)
     assert command == "dice"
     assert params == ""
 
-    content = "!c:"
+    content = "!c/"
     try:
         command, params = ds.parse(content)
         assert False, "Should have raised a BadCommandError."
